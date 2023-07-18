383081395ea202d4c573bdd7b8085e9a3447bfb6
alisa-po <alisa.popova21@gmail.com>
2023-07-18 18:15:49 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log('Hello, Node!');
+console.log('Goodbye!');
+
+const os = require('os');
+console.log('Type: ' + os.type());
+
+const assert = require('assert');
+console.log('Assert' + assert());
