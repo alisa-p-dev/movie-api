70180ed80b30af31978101de44e7c55e4d1e31f7
alisa-po <alisa.popova21@gmail.com>
2023-07-20 13:09:27 +0200

    dependencies added

--- test.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello, Node!');
-console.log('Goodbye!');
-
-const os = require('os');
-console.log('Type: ' + os.type());
-
-const assert = require('assert');
-console.log('Assert' + assert());
