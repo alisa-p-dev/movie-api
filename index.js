b5df87085582d07ad327fbccd8423b3a20e11392
alisa-po <alisa.popova21@gmail.com>
2023-07-23 20:43:47 +0200

    added error-handling middleware function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express"),
   morgan = require("morgan");
+  const path = require("path");
 
 const app = express();
 
@@ -34,27 +35,32 @@ let topMovies = [
   { title: "A Beautiful Mind", year: "2001" },
 ];
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // GET requests
 app.get("/", (req, res) => {
   res.send("Welcome to myFlix app!");
 });
 
 app.get("/documentation", (req, res) => {
-  res.sendFile("/documentation.html", { root: __dirname });
+  res.sendFile("documentation.html", { root: path.join(__dirname, "public") });
 });
 
 app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
-app.use(express.static('public'));
+// Error handling middleware
+app.use((err, req, res, next) => {
+  // Log the error to the terminal
+  console.error("Application-level error:", err.stack);
+
+  // Respond with a 500 Internal Server Error to the client
+  res.status(500).send("Something went wrong on the server!");
+});
 
 // listen for requests
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
