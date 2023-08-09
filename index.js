b2e88645591cc5b618cdb1fa21ebc7834b56d20a
alisa-po <alisa.popova21@gmail.com>
2023-08-10 01:03:04 +0200

    connect db to an API

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const express = require("express"),
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', 
-{ useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', 
+// { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
